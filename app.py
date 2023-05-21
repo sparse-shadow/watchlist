711556d6328482bdacdd407a2aa236e0d866cf2f
sparse <2835774984@qq.com>
2023-05-21 08:25:49 +0800

    Add base template and error template

--- app.py
+++ app.py
@@ -87,6 +87,17 @@ class Movie(db.Model):
 
 @app.route('/')
 def index():
-    user = User.query.first()    # 读取用户记录
+    # user = User.query.first()    # 读取用户记录
     movies = Movie.query.all()  # 读取所有电影记录
-    return render_template('index.html', user=user, movies=movies)
+    # return render_template('index.html', user=user, movies=movies)
+    return render_template('index.html', movies=movies)
+@app.errorhandler(404)  # 传入要处理的错误代码
+def page_not_found(e):  # 接受异常对象作为参数
+    user = User.query.first()
+    # return render_template('404.html', user=user), 404  # 返回模板和状态码
+    return dict(user=user)
+
+@app.context_processor
+def inject_user():  # 函数名可以随意修改
+    user = User.query.first()
+    return dict(user=user)  # 需要返回字典，等同于 return {'user': user}

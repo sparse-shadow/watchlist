4890903dda2d81edb648e3a75bbc1bd40fd2a679
sparse <2835774984@qq.com>
2023-06-10 16:35:59 +0800

    Ready to deploy

--- app.py
+++ app.py
@@ -245,3 +245,6 @@ def settings():
         return redirect(url_for('index'))
 
     return render_template('settings.html')
+
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'dev')
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(os.path.dirname(app.root_path), os.getenv('DATABASE_FILE', 'data.db'))
